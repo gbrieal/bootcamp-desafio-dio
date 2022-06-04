f02cfdf70f2f9b8bedaed1ceb7e31f2e02c8e53c
gbrieal <gabrielgalvaoo@gmail.com>
2022-06-03 21:56:50 -0300

    Abstração e Encapsulamento

---
+++ br.com.dio.desafio/lib/Main.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Desrição do Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Desrição do Curso JavaScript");
+        curso2.setCargaHoraria(6);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição da Mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+    }
+}
