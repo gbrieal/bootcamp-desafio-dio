6da62fca9fceab340c636043a31bdf13671f1646
gbrieal <gabrielgalvaoo@gmail.com>
2022-06-03 22:56:21 -0300

    Criando Classes de Domínio

--- br.com.dio.desafio/br.com.dio.desafio.dominio/Dev.java
+++ br.com.dio.desafio/br.com.dio.desafio.dominio/Dev.java
@@ -1,4 +1,5 @@
 import java.util.LinkedHashSet;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -8,12 +9,25 @@ public class Dev {
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
     }
 
     public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
     }
 
-    public void calcularXp() {
+    public double calcularXp() {
+        return this.conteudosConcluidos
+                .stream()
+                .mapToDouble(conteudo -> conteudo.calcularXp())
+                .sum();
     }
 
     public String getNome() {
