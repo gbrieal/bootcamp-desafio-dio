2385a03419b76223fc9da55a2cb9f22a7ef49fc5
gbrieal <gabrielgalvaoo@gmail.com>
2022-06-03 22:24:46 -0300

    Herança e Polimorfismo 1

--- br.com.dio.desafio/br.com.dio.desafio.dominio/Main.java
+++ br.com.dio.desafio/br.com.dio.desafio.dominio/Main.java
@@ -1,8 +1,10 @@
+
 import java.time.LocalDate;
 
 public class Main {
 
     public static void main(String[] args) {
+
         Curso curso1 = new Curso();
         curso1.setTitulo("Curso Java");
         curso1.setDescricao("Desrição do Curso Java");
@@ -13,6 +15,8 @@ public class Main {
         curso2.setDescricao("Desrição do Curso JavaScript");
         curso2.setCargaHoraria(6);
 
+        //Conteudo conteudo = new Curso(); -> Polimorfismo
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descrição da Mentoria de Java");
@@ -22,6 +26,6 @@ public class Main {
         System.out.println(curso2);
         System.out.println(mentoria);
 
-
+        // Conteudo conteudo = new Conteudo(); -> Não é possível instanciar porque a classe é do tipo Abstract
     }
 }
